eb85b15b9dda1c3844c0ca64d34d7bb0debf916e
Adham Nour <adhamnourelwaffaa@gmail.com>
2024-03-23 11:13:13 +0200

    finished status

--- Genera_popularity_overtime.sql
+++
@@ -1,9 +0,0 @@
-select
-	geners_jt.name,
-	max(popularity) as popularity 
-from
-	movies m
-cross join json_table(m.genres, '$[*]' columns(id int path '$.id', name varchar(255) path '$.name')) geners_jt
-group by geners_jt.name
-order by popularity desc
-;
